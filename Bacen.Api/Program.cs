99f9a575ceb0243b255a9f43fb2c3a95730bddc2
Priscila <priscilagrazieleba@gmail.com>
2022-12-03 12:56:42 -0300

    updates

--- Bacen.Api/Program.cs
+++ Bacen.Api/Program.cs
@@ -10,7 +10,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "Bacen API", Version = "v1" });
+});
+
 builder.Services.AddAutoMapper(typeof(Program).Assembly);
 builder.Services.AddScoped<IClientService, ClientService>();
 builder.Services.AddScoped<IValidator<Client>, ClientValidator>();
@@ -20,6 +24,8 @@ builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.Configure<BacenDatabaseSettings>(
     builder.Configuration.GetSection("BacenDatabase")
 );
+
+builder.Services.AddHealthChecks();
 
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
@@ -31,4 +37,5 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers().WithOpenApi();
-app.Run();
+app.MapHealthChecks("/health");
+app.Run("http://*:80");
